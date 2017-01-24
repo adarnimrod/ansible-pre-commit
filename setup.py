3c3ee98799a0b735f535e636994eeac61f5485a9
Adar Nimrod <nimrod@shore.co.il>
2017-01-24 16:49:46 +0200

    - Pass setup.py check.

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from setuptools import setup
 
 setup(
         name='pre_commit_ansible_dummy_package',
+        url='https://www.shore.co.il/git/ansible-pre-commit',
+        author='Nimrod Adar',
+        author_email='nimrod@shore.co.il',
         version='0.1.0',
         install_requires=['ansible==2.2.1.0'],
 )
